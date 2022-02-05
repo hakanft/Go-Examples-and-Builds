bf79fd65e83124fce5dc12a028c376fc852ba2d6
HFT <82724980+hakanft@users.noreply.github.com>
2022-02-05 10:22:43 +0300

    Add files via upload

---
+++ 03Variables.go
@@ -0,0 +1,28 @@
+/*package main
+
+import "fmt"
+
+func main () {
+
+  var a string = "initial"
+
+  var b,c int = 1,2
+
+  var d bool = false
+
+  e := 3.2
+
+  var f int
+
+  fmt.Println(a)
+  fmt.Println(b)
+  fmt.Println(c)
+  fmt.Println(d)
+  fmt.Println(e)
+  fmt.Println(f)
+}*/
+
+
+
+
+
---
+++ 05For.go
@@ -0,0 +1,30 @@
+/*package main
+
+import "fmt"
+
+func main() {
+
+  for i := 0; i <3; i++{
+    fmt.Println(i)
+  
+  }
+
+  j := 0
+  for j<4{
+    fmt.Println("this is j value: ",j)
+    j++
+  }
+
+  for {
+    fmt.Println("Loop")
+    break
+  }
+
+  for n := 0; n < 10; n++{
+    if n%2 ==0{
+      continue
+    }
+    fmt.Println(n)
+  }
+  
+}*/
